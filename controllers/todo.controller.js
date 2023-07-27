6b6fc3d1fcd044bc6103d83b67e27ee910d4fec1
Amandeep Singh <programmeramandeep@gmail.com>
2023-07-27 19:58:09 +0530

    Test if response is sending back JSON body

--- controllers/todo.controller.js
+++ controllers/todo.controller.js
@@ -1,8 +1,8 @@
 const TodoModel = require('../models/todo.model');
 
 const createTodo = (req, res, next) => {
-  TodoModel.create(req.body);
-  res.status(201).send();
+  const createModel = TodoModel.create(req.body);
+  res.status(201).json(createModel);
 };
 
 module.exports = { createTodo };
