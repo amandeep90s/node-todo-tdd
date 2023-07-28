04bbd51b4cb061ca253a6b1ff10d218d8b316066
Amandeep Singh <programmeramandeep@gmail.com>
2023-07-28 17:07:15 +0530

    Creating our rexpress router

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const todoRoutes = require('./routes/todo.routes');
 
 const app = express();
 
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
+
+app.use('/todos', todoRoutes);
 
 app.get('/', (req, res, next) => {
   res.send('<h1>Hello world</h1>');
---
+++ routes/todo.routes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const TodoController = require('../controllers/todo.controller');
+
+const router = express.Router();
+
+router.post('/', TodoController.createTodo);
+
+module.exports = router;
