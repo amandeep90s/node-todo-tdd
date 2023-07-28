c64d1f51d22e6d6bf87bdd16fffc5822e7a65448
Amandeep Singh <programmeramandeep@gmail.com>
2023-07-28 20:26:07 +0530

    Modify our unit tests to use async/await

--- app.js
+++ app.js
@@ -16,8 +16,8 @@ app.get('/', (req, res, next) => {
   res.json({ data: 'Hello world' });
 });
 
-app.listen(3000, () => {
-  console.log('App is running on http://localhost:3000');
-});
+// app.listen(3000, () => {
+//   console.log('App is running on http://localhost:3000');
+// });
 
 module.exports = app;
